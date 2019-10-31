23f351e0305a411c4f3bf920a29a4f86efa4b57b
smittytone <smittytone@icloud.com>
2019-10-31 11:31:12 +0000

    Add Credits menu, links to source sites

--- PreviewMarkdown/AppDelegate.swift
+++ PreviewMarkdown/AppDelegate.swift
@@ -11,7 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    // Menu Items Tab
+    @IBOutlet var creditMenuPM: NSMenuItem!
+    @IBOutlet var creditMenuDiscount: NSMenuItem!
+    @IBOutlet var creditMenuQLMarkdown: NSMenuItem!
     
+    // Windows
     @IBOutlet weak var window: NSWindow!
 
 
@@ -26,6 +31,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         // Close the window... which will trigger an app closure
         self.window.close()
+    }
+    
+    
+    @IBAction @objc func doShowSites(sender: Any) {
+        
+        // Open the websites for contributors
+        let item: NSMenuItem = sender as! NSMenuItem
+        var path: String = "https://smittytone.github.io/index.html"
+        
+        if item == self.creditMenuDiscount {
+            path = "https://github.com/Orc/discount"
+        } else if item == self.creditMenuQLMarkdown {
+            path = "https://github.com/toland/qlmarkdown"
+        }
+        
+        // Open the selected website
+        NSWorkspace.shared.open(URL.init(string:path)!)
     }
 }
 
