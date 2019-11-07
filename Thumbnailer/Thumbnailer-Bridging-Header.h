4a5f780f8f062f63b5384b6f5e369f991db1d58c
smittytone <smittytone@icloud.com>
2019-11-07 15:25:17 +0000

    Update Thumbnailer-Bridging-Header.h

--- Thumbnailer/Thumbnailer-Bridging-Header.h
+++ Thumbnailer/Thumbnailer-Bridging-Header.h
@@ -2,6 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "discount-support.h"
+// NOTE Uncommented while we're not working with WKWebView
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "discount-support.h"
