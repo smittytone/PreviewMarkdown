c8d0e9ca314f66deb50400be1b71cb8d958c263c
Tony Smith (smittytone) <tony@smittytone.net>
2025-01-08 16:26:08 +0000

    Add NSColor from hex string

--- Common/Extensions.swift
+++ Common/Extensions.swift
@@ -3,21 +3,20 @@
  *  Code common to Previewer and Thumbnailer: Swift Extensions
  *
  *  Created by Tony Smith on 17/09/2024.
- *  Copyright © 2024 Tony Smith. All rights reserved.
+ *  Copyright © 2025 Tony Smith. All rights reserved.
  */
 
 import Foundation
 import AppKit
 
 
-/**
-Get the encoding of the string formed from data.
-
-- Returns: The string's encoding or nil.
-*/
-
 extension Data {
     
+    /**
+    Get the encoding of the string formed from data.
+
+    - Returns: The string's encoding or nil.
+    */
     var stringEncoding: String.Encoding? {
         var nss: NSString? = nil
         guard case let rawValue = NSString.stringEncoding(for: self,
@@ -29,16 +28,16 @@ extension Data {
 }
 
 
-/**
-Swap the paragraph style in all of the attributes of
- an NSMutableAttributedString.
-
-- Parameters:
- - paraStyle: The injected NSParagraphStyle.
-*/
 extension NSMutableAttributedString {
     
+    ///
+    /// Swap the paragraph style in all of the attributes of an NSMutableAttributedString.
+    ///
+    /// - Parameters:
+    /// - paraStyle: The injected NSParagraphStyle.
+    ///
     func addParaStyle(with paraStyle: NSParagraphStyle) {
+
         beginEditing()
         self.enumerateAttribute(.paragraphStyle, in: NSRange(location: 0, length: self.length)) { (value, range, stop) in
             if let _ = value as? NSParagraphStyle {
@@ -46,5 +45,100 @@ extension NSMutableAttributedString {
             }
         }
         endEditing()
+    }
+}
+
+
+extension Scanner {
+    
+    func getNextCharacter(in outer: String) -> String {
+        
+        let string: NSString = self.string as NSString
+        let idx: Int = self.currentIndex.utf16Offset(in: outer)
+        let nextChar: String = string.substring(with: NSMakeRange(idx, 1))
+        return nextChar
+    }
+}
+
+
+extension CGFloat {
+    
+    /**
+     Determine if the instance is near enough the specified value as makes no odds.
+     
+     - Parameters:
+        - value: The float value we're comparing the instance to.
+     
+     - Returns:
+        `true` if the values are proximate, otherwise `false`.
+     */
+    func isClose(to value: CGFloat) -> Bool {
+
+        let absA: CGFloat = abs(self)
+        let absB: CGFloat = abs(value)
+        let diff: CGFloat = abs(self - value)
+        
+        if self == value {
+            return true
+        } else if self == .zero || value == .zero || (absA + absB) < Self.leastNormalMagnitude {
+            return diff < Self.ulpOfOne * Self.leastNormalMagnitude
+        } else {
+            return (diff / Self.minimum(CGFloat(absA + absB), Self.greatestFiniteMagnitude)) < .ulpOfOne
+        }
+    }
+}
+
+
+extension NSColor {
+    
+    static func colourFromHexString(_ colourValue: String) -> NSColor {
+        
+        var colourString: String = colourValue.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+        if (colourString.hasPrefix("#")) {
+            // The colour is defined by a hex value
+            colourString = (colourString as NSString).substring(from: 1)
+        }
+        
+        // Colours in hex strings have 3, 6 or 8 (6 + alpha) values
+        if colourString.count != 8 && colourString.count != 6 && colourString.count != 3 {
+            return NSColor.gray
+        }
+
+        var r: UInt64 = 0, g: UInt64 = 0, b: UInt64 = 0, a: UInt64 = 0
+        var divisor: CGFloat
+        var alpha: CGFloat = 1.0
+
+        if colourString.count == 6 || colourString.count == 8 {
+            // Decode a six-character hex string
+            let rString: String = (colourString as NSString).substring(to: 2)
+            let gString: String = ((colourString as NSString).substring(from: 2) as NSString).substring(to: 2)
+            let bString: String = ((colourString as NSString).substring(from: 4) as NSString).substring(to: 2)
+
+            Scanner(string: rString).scanHexInt64(&r)
+            Scanner(string: gString).scanHexInt64(&g)
+            Scanner(string: bString).scanHexInt64(&b)
+
+            divisor = 255.0
+            
+            if colourString.count == 8 {
+                // Decode the eight-character hex string's alpha value
+                let aString: String = ((colourString as NSString).substring(from: 6) as NSString).substring(to: 2)
+                Scanner(string: aString).scanHexInt64(&a)
+                alpha = CGFloat(a) / divisor
+            }
+        } else {
+            // Decode a three-character hex string
+            let rString: String = (colourString as NSString).substring(to: 1)
+            let gString: String = ((colourString as NSString).substring(from: 1) as NSString).substring(to: 1)
+            let bString: String = ((colourString as NSString).substring(from: 2) as NSString).substring(to: 1)
+
+            Scanner(string: rString).scanHexInt64(&r)
+            Scanner(string: gString).scanHexInt64(&g)
+            Scanner(string: bString).scanHexInt64(&b)
+            divisor = 15.0
+        }
+
+        return NSColor(red: CGFloat(r) / divisor, green: CGFloat(g) / divisor, blue: CGFloat(b) / divisor, alpha: alpha)
     }
 }
