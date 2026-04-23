57524204a20094821916c3eacce366888015d951
Tony Smith (smittytone) <tony@smittytone.net>
2026-04-23 17:41:56 +0100

    Update

--- RenderDemo/AppDelegate.swift
+++ RenderDemo/AppDelegate.swift
@@ -23,38 +23,50 @@ class AppDelegate:  NSObject,
     @IBOutlet var previewTextView: NSTextView!
     @IBOutlet var previewScrollView: NSScrollView!
     @IBOutlet var modeButton: NSButton!
-    @IBOutlet var modeIndicator: NSImageView!
+    //@IBOutlet var modeIndicator: NSImageView!
+    @IBOutlet weak var reloadButton: NSButton!
+    @IBOutlet weak var reloadMenuItem: NSMenuItem!
+    @IBOutlet weak var progress: NSProgressIndicator!
+    @IBOutlet weak var thumbButton: NSButton!
 
 
     // MARK: - Private Properies
 
-    private var openDialog: NSOpenPanel? = nil
-    private var currentURL: URL? = nil
+    private var _currentURL: URL? = nil
+    private var currentDirURL: URL? = nil
     private var renderAsDark: Bool = true
     private var renderIndents: Bool = false
 
-    
+    private var currentURL: URL? {
+        get {
+            return self._currentURL
+        }
+        set(new) {
+            self._currentURL = new
+            self.reloadButton.isEnabled = new != nil
+            self.reloadMenuItem.isEnabled = new != nil
+        }
+    }
+
+
     // MARK: - Class Lifecycle Functions
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         
         // Set the mode button
         self.modeButton.state = self.renderAsDark ? .on : .off
+        self.reloadButton.isEnabled = false
+        self.reloadMenuItem.isEnabled = false
+        self.progress.isHidden = true
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.doRender),
+                                               name: NSNotification.Name(rawValue: "com.bps.rd.load"),
+                                               object: nil)
 
         // Centre the main window and display
         self.window.center()
         self.window.makeKeyAndOrderFront(self)
-        
-        var currentlyInLightMode: Bool = true
-        if #available(macOS 11.0, *) {
-            let appearance: NSAppearance = NSAppearance.currentDrawing()
-            currentlyInLightMode = appearance.name == NSAppearance.Name.aqua
-        } else {
-            let appearance: NSAppearance = NSAppearance.current
-            currentlyInLightMode = appearance.name == NSAppearance.Name.aqua
-        }
-
-        self.modeIndicator.contentTintColor = currentlyInLightMode ? .systemGreen : .systemPurple
     }
     
     
@@ -69,39 +81,34 @@ class AppDelegate:  NSObject,
     
     @IBAction private func doLoadFile(_ sender: Any) {
 
-        self.openDialog = NSOpenPanel.init()
-        self.openDialog!.canChooseFiles = true
-        self.openDialog!.canChooseDirectories = false
-        self.openDialog!.allowsMultipleSelection = false
-        self.openDialog!.delegate = self
-        self.openDialog!.directoryURL = URL.init(fileURLWithPath: "")
-        
-        if let uti = UTType.init("net.daringfireball.markdown") {
-            self.openDialog!.allowedContentTypes = [uti]
-        }
-
-        if let uti = UTType.init("net.ia.markdown") {
-            self.openDialog!.allowedContentTypes.append(uti)
-        }
-
-        if self.openDialog!.runModal() == .OK {
-            self.currentURL = self.openDialog!.url
+        let openPanel = NSOpenPanel()
+        openPanel.delegate = self
+        openPanel.canChooseFiles = true
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        if self.currentDirURL != nil {
+            openPanel.directoryURL = self.currentDirURL!
         } else {
-            self.currentURL = nil
+            openPanel.directoryURL = FileManager.default.homeDirectoryForCurrentUser
         }
 
-        self.openDialog!.close()
-        self.openDialog = nil
-        
-        if let openURL = self.currentURL {
-            if let possibleError = renderContent(openURL) {
-                let errorAlert: NSAlert = NSAlert.init(error: possibleError)
-                errorAlert.beginSheetModal(for: self.window)
+        openPanel.beginSheetModal(for: self.window) { (response) in
+            if response == .OK {
+                self.currentURL = openPanel.url
+                self.currentDirURL = openPanel.directoryURL
+                NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "com.bps.rd.load")))
             }
         }
     }
     
-    
+
+    @IBAction
+    private func doReloadFile(_ sender: Any) {
+
+        doRender(Notification(name: Notification.Name(rawValue: "")))
+    }
+
+
     @IBAction private func doSwitchMode(_ sender: Any) {
 
         self.renderAsDark = self.modeButton.state == .on
@@ -109,21 +116,47 @@ class AppDelegate:  NSObject,
     }
 
 
-    @IBAction private func doReRenderFile(_ sender: Any) {
+    @IBAction
+    private func doReRenderFile(_ sender: Any) {
 
-        let possibleError: NSError? = renderContent(self.currentURL)
-        if possibleError != nil {
-            // Pop up an alert
-            let errorAlert: NSAlert = NSAlert.init(error: possibleError!)
-            errorAlert.beginSheetModal(for: self.window)
+        wenderFile()
+    }
+
+
+    @objc
+    func wenderFile() {
+
+        Task { @MainActor in
+            self.progress.isHidden = false
+            self.progress.startAnimation(self)
+
+            let possibleError: NSError? = await renderContent(self.currentURL)
+            self.progress.stopAnimation(self)
+
+            if possibleError != nil {
+                // Pop up an alert
+                let errorAlert: NSAlert = NSAlert(error: possibleError!)
+                await errorAlert.beginSheetModal(for: self.window)
+            }
         }
     }
 
 
     // MARK: - Rendering Functions
-    
-    func renderContent(_ fileToRender: URL?) -> NSError? {
-        
+
+    @objc
+    private func doRender(_ note: Notification) {
+
+        self.progress.isHidden = false
+        self.progress.startAnimation(self)
+        let _ = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.wenderFile), userInfo: nil, repeats: false)
+
+    }
+
+
+    @MainActor
+    func renderContent(_ fileToRender: URL?) async -> NSError? {
+
         var reportError: NSError? = nil
         
         do {
@@ -131,7 +164,7 @@ class AppDelegate:  NSObject,
                 self.window.title = mdUrl.absoluteString
 
                 // Get the file contents as a string
-                let data: Data = try Data.init(contentsOf: mdUrl, options: [.uncached])
+                let data = try Data.init(contentsOf: mdUrl, options: [.uncached])
 
                 // Get the string's encoding, or fail back to .utf8
                 let encoding: String.Encoding = data.stringEncoding ?? .utf8
@@ -160,37 +193,34 @@ class AppDelegate:  NSObject,
                     let mdAttString: NSAttributedString = common.getAttributedString(mdString[...])
 
                     if let renderTextStorage: NSTextStorage = self.previewTextView.textStorage {
-                        safeMainSync {
-                            self.previewTextView.backgroundColor = common.settings.doReverseMode ? NSColor.init(white: 1.0, alpha: 0.9) : NSColor.textBackgroundColor
-                            self.previewScrollView.scrollerKnobStyle = common.settings.doReverseMode ? .dark : .light
+                        self.previewTextView.backgroundColor = common.settings.doReverseMode ? NSColor.init(white: 1.0, alpha: 0.9) : NSColor.textBackgroundColor
+                        self.previewScrollView.scrollerKnobStyle = common.settings.doReverseMode ? .dark : .light
 
-                            // Correct way to set a text view's link colouring, etc. - and have it stick
-                            self.previewTextView.linkTextAttributes = [NSAttributedString.Key.foregroundColor: common.linkColor,
-                                                                      NSAttributedString.Key.cursor: NSCursor.pointingHand ]
+                        // Correct way to set a text view's link colouring, etc. - and have it stick
+                        self.previewTextView.linkTextAttributes = [NSAttributedString.Key.foregroundColor: common.linkColor,
+                                                                  NSAttributedString.Key.cursor: NSCursor.pointingHand ]
 
-                            // Auto-scroll to top of preview
-                            self.previewScrollView.contentView.scroll(to: NSMakePoint(0, 0))
-                            self.previewScrollView.reflectScrolledClipView(self.previewScrollView.contentView)
-                            
-                            // We need to access the NSTextView's containter to apply the custom NSLayoutManager
-                            if let renderTextContainer: NSTextContainer = self.previewTextView.textContainer {
-                                let layouter = PMLayouter()
-                                layouter.fontSize = common.settings.fontSize
-                                layouter.marginDelta = common.settings.doShowMargin ? BUFFOON_CONSTANTS.PREVIEW_SIZE.PREVIEW_MARGIN_WIDTH : 0.0
-                                renderTextContainer.replaceLayoutManager(layouter)
-                            }
-                            
-                            renderTextStorage.beginEditing()
-                            renderTextStorage.setAttributedString(mdAttString)
-                            renderTextStorage.endEditing()
-                            self.previewTextView.display()
+                        // Auto-scroll to top of preview
+                        self.previewScrollView.contentView.scroll(to: NSMakePoint(0, 0))
+                        self.previewScrollView.reflectScrolledClipView(self.previewScrollView.contentView)
+
+                        // We need to access the NSTextView's containter to apply the custom NSLayoutManager
+                        if let renderTextContainer: NSTextContainer = self.previewTextView.textContainer {
+                            let layouter = PMLayouter()
+                            layouter.fontSize = common.settings.fontSize
+                            layouter.marginDelta = common.settings.doShowMargin ? BUFFOON_CONSTANTS.PREVIEW_SIZE.PREVIEW_MARGIN_WIDTH : 0.0
+                            renderTextContainer.replaceLayoutManager(layouter)
                         }
-                        
+
+                        renderTextStorage.beginEditing()
+                        renderTextStorage.setAttributedString(mdAttString)
+                        renderTextStorage.endEditing()
+                        self.previewTextView.display()
                         return nil
                     }
 
                     // We can't access the preview NSTextView's NSTextStorage
-                    reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.BAD_TS_STRING)
+                    reportError = makeError(BUFFOON_CONSTANTS.ERRORS.CODES.BAD_TS_STRING)
                 } else {
                     // We couldn't convert to data to a valid encoding
                     let errDesc: String = "\(BUFFOON_CONSTANTS.ERRORS.MESSAGES.BAD_TS_STRING) \(encoding)"
@@ -207,7 +237,7 @@ class AppDelegate:  NSObject,
             }
         } catch {
             // We couldn't read the file so set an appropriate error to report back
-            reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.FILE_WONT_OPEN)
+            reportError = makeError(BUFFOON_CONSTANTS.ERRORS.CODES.FILE_WONT_OPEN)
         }
         
         return reportError
@@ -224,7 +254,7 @@ class AppDelegate:  NSObject,
 
      - Returns: The described error as an NSError.
      */
-    func setError(_ code: Int) -> NSError {
+    func makeError(_ code: Int) -> NSError {
 
         var errDesc: String
 
@@ -244,22 +274,6 @@ class AppDelegate:  NSObject,
         return NSError(domain: BUFFOON_CONSTANTS.APP_CODE_PREVIEWER,
                        code: code,
                        userInfo: [NSLocalizedDescriptionKey: errDesc])
-    }
-    
-    
-    /**
-     Execute the supplied block on the main thread.
-    */
-    private func safeMainSync(_ block: @escaping ()->()) {
-
-        if Thread.isMainThread {
-            block()
-        } else {
-            Task {
-                @MainActor in
-                    block()
-            }
-        }
     }
 
 }
